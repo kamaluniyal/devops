85b952da2514e15947abd6add77bc8031cfb002a
Ubuntu <ubuntu@ip-10-0-0-189.ec2.internal>
2021-03-08 04:53:29 +0000

    commiting upload docker left earlier

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,1 +1,19 @@
-#test
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="kamaluniyal/pybuild1"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag pybuild1 $dockerpath
+docker login -u kamaluniyal
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
