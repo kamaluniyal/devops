e289d6e263ffdc2f1673607596595187abe22428
Ubuntu <ubuntu@ip-10-0-0-189.ec2.internal>
2021-03-08 04:56:48 +0000

    kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,1 +1,16 @@
-#test
+
+# Step 1:
+# dockerpath=<>
+dockerpath="kamaluniyal/pybuild1"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run pybuild1 --image=$dockerpath 
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward pybuild1 8000:80 
